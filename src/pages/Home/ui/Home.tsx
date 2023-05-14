1b3bdfad66ef2152f3cb2541a4db63a28afbb310
arozhdan <halala30@gmail.com>
2023-05-14 16:51:08 +0200

    Remove Bug Button from main page

--- src/pages/Home/ui/Home.tsx
+++ src/pages/Home/ui/Home.tsx
@@ -1,11 +1,9 @@
 import { useTranslation } from 'react-i18next';
-import { BugButton } from 'widgets/PageError';
 
 export default () => {
     const { t } = useTranslation();
     return (
         <div>
-            <BugButton />
             <h1>{t('home')}</h1>
         </div>
     );
